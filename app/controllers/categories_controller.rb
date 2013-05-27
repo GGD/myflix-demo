c84c7f086dedf367a3c1509ded51bb0477b4b285
GGD <ggd747@gmail.com>
2013-05-27 21:54:20 +0800

    HW 0.8 Show category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,4 +2,9 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.includes(:videos).all
   end
+
+  def show
+    @category = Category.find(params[:id])
+    @videos = @category.videos.all
+  end
 end
