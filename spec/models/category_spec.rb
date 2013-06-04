9b86889f83da6a4e27110696f0bc01026b42f259
GGD <ggd747@gmail.com>
2013-06-04 17:26:29 +0800

    HW13 Use shoulda matchers

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,21 +1,11 @@
 require 'spec_helper'
 
 describe Category do
-  before :each do
-    @category = Category.new(name: 'TV Commedies')
-  end
-
   it "save itself" do
-    @category.save
+    category = Category.new(name: 'TV Commedies')
+    category.save
     Category.first.name.should == 'TV Commedies'
   end
 
-  it "has many videos" do
-    @category.save
-    video1 = Video.new(title: 'Monk', description: 'Moooooonk', large_cover: '/tmp/monk_large.jpg', small_cover: '/tmp/monk.jpg', category_id: '1')
-    video2 = Video.new(title: 'Terminator', description: 'Ill be back', large_cover: '/tmp/monk_large.jpg', small_cover: '/tmp/monk.jpg', category_id: '1')
-    video1.save
-    video2.save
-    @category.videos.size.should == 2
-  end
+  it { should have_many(:videos) }
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,29 +1,13 @@
 require 'spec_helper'
 
 describe Video do
-  before :each do
-    @video = Video.new(title: 'Monk', description: 'Moooooonk', large_cover: '/tmp/monk_large.jpg', small_cover: '/tmp/monk.jpg', category_id: '1')
-  end
-
   it "save itself" do
-    @video.save
+    video = Video.new(title: 'Monk', description: 'Moooooonk', large_cover: '/tmp/monk_large.jpg', small_cover: '/tmp/monk.jpg', category_id: '1')
+    video.save
     Video.first.title.should == 'Monk'
   end
 
-  it "belongs to category" do
-    category = Category.new(name: 'TV Commedies')
-    category.save
-    @video.save
-    @video.category_id.should == category.id
-  end
-
-  it "require a title" do
-    @video.title = nil
-    @video.should_not be_valid
-  end
-
-  it "require a description" do
-    @video.description = nil
-    @video.should_not be_valid
-  end
+  it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 end
