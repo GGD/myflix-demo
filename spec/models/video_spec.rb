fc4f79515fde1d3eefd404ea886f8946886ef7bb
GGD <ggd747@gmail.com>
2013-06-06 22:12:32 +0800

    use describe to test a method instead of context

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -5,7 +5,7 @@ describe Video do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
-  context "#self.search_by_title" do
+  describe "#self.search_by_title" do
     before :each do
       Video.create(title: 'Monk', description: 'Moooonk', large_cover: '/tmp/monk_large.jpg', small_cover: '/tmp/monk.jpg', category_id: '1')
       Video.create(title: 'SouthPark', description: 'south_park', large_cover: '/tmp/south_pard.jpg', small_cover: '/tmp/south_park.jpg', category_id: '2')
