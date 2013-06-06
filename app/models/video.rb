b5647de89d024699b387071d374318489c6554ff
GGD <ggd747@gmail.com>
2013-06-06 21:22:05 +0800

    HW15 Implement 'search_by_title' on the video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,8 @@ class Video < ActiveRecord::Base
   attr_accessible :description, :large_cover, :small_cover, :title, :category_id
 
   validates_presence_of :title, :description
+
+  def self.search_by_title(search_term)
+    Video.where('title LIKE ?', "%#{search_term}%")
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,4 +4,21 @@ describe Video do
   it { should belong_to(:category) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
+
+  context "#self.search_by_title" do
+    before :each do
+      Video.create(title: 'Monk', description: 'Moooonk', large_cover: '/tmp/monk_large.jpg', small_cover: '/tmp/monk.jpg', category_id: '1')
+      Video.create(title: 'SouthPark', description: 'south_park', large_cover: '/tmp/south_pard.jpg', small_cover: '/tmp/south_park.jpg', category_id: '2')
+    end
+
+    it "return empty array if video is not found" do
+      search_term = "Superman"
+      expect(Video.search_by_title(search_term)).to eq([])
+    end
+    it "return videos if title is matched with searching string" do
+      search_term = "k"
+      videos = Video.all
+      expect(Video.search_by_title(search_term)).to eq(videos)
+    end
+  end
 end
