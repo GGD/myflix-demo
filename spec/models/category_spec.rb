19d7ae440df6940e6ddd609c4f93b260adf26cdd
GGD <ggd747@gmail.com>
2013-06-06 20:59:07 +0800

    remove test:'save itself'

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,11 +1,5 @@
 require 'spec_helper'
 
 describe Category do
-  it "save itself" do
-    category = Category.new(name: 'TV Commedies')
-    category.save
-    Category.first.name.should == 'TV Commedies'
-  end
-
   it { should have_many(:videos) }
 end
