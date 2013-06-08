f863de36ccf9e922aef7275fc6db5a8f283ab06d
GGD <ggd747@gmail.com>
2013-06-08 22:58:22 +0800

    add session[:user_id] in users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
+      session[:user_id] = @user.id
       redirect_to categories_path
     else
       render :new
