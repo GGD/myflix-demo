ff31a0f1077a04905415e8512b4e12c0093c6cb3
GGD <ggd747@gmail.com>
2013-06-13 11:15:21 +0800

    one assertion per spec for users_controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,22 +9,39 @@ describe UsersController do
   end
 
   describe "POST create" do
-    it "assigns @user" do
-      post :create, user: { email: nil, password: '1234', full_name: 'Ga Dii' }
-      expect(assigns(:user)).to be_new_record
-      expect(assigns(:user)).to be_instance_of(User)
+    context "with valid inputs" do
+      before { post :create, user: Fabricate.attributes_for(:user) }
+      
+      it "creates the user" do
+        expect(User.count).to eq(1)
+      end
+
+      it "puts the user in session" do
+        expect(session[:user_id]).to eq(User.first.id)
+      end
+
+      it "redirects to categories_path" do
+        expect(response).to redirect_to categories_path
+      end
     end
 
-    it "create user and redirect to categories_path when params is valid" do
-      expect{ post :create, user: Fabricate.attributes_for(:user) }.to change(User, :count).by(1)
-      expect(response).to redirect_to categories_path
-    end
+    context "with invalid inputs" do
+      before do
+        post :create, user: { password: '1234', full_name: 'Ga Dii' }
+      end
 
-    it "render :new template when create user failed" do
-      post :create, user: { email: nil, password: '1234', full_name: 'Ga Dii' }
-      expect(User.count).to eq(0)
+      it "assigns @user" do
+        expect(assigns(:user)).to be_new_record
+        expect(assigns(:user)).to be_instance_of(User)
+      end
 
-      expect(response).to render_template :new
+      it "does not create the user" do
+        expect(User.count).to eq(0)
+      end
+
+      it "render :new template" do
+        expect(response).to render_template :new
+      end
     end
   end
 end 
