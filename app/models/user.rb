02381ff851940d4810063a3445417dbc36703379
GGD <ggd747@gmail.com>
2013-06-20 09:43:08 +0800

    HW5.2 Push down complexity of video ordering in queue to the model level

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,12 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :email
   validates_presence_of :email, :password, :full_name
+  
   has_secure_password
+
+  def normalize_queue_item_positions
+    queue_items.each_with_index do |queue_item, index|
+      queue_item.update_attributes(position: index+1)
+    end
+  end
 end
