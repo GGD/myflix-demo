e65ab6a0b1e2171149a5e91cc68008fff74f99b3
GGD <ggd747@gmail.com>
2013-06-28 09:59:18 +0800

    HW6.3 A more complicated feature spec

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -3,10 +3,7 @@ require 'spec_helper'
 feature "User signs in" do
   scenario "with existing email and password" do
     tifa = Fabricate(:user)
-    visit sign_in_path
-    fill_in 'Email Address', with: tifa.email
-    fill_in 'Password', with: tifa.password
-    click_button 'Sign in'
+    sign_in(tifa)
     expect(page).to have_content tifa.full_name
   end
 end
